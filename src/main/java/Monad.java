851a28612f0245a3ec76edf5f4e82125917b910b
Achiack <70217903+Achiack@users.noreply.github.com>
2026-01-20 22:27:05 +0800

    chatbot now echos commands entered by the user, and exits when the user types the command "bye"

--- src/main/java/Monad.java
+++ src/main/java/Monad.java
@@ -1,16 +1,32 @@
+import java.util.Scanner;
+
 public class Monad {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
         String ChatbotName = "Monad";
-        System.out.println("--------------------------");
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("___________________________");
         System.out.println("Hello! I'm " + ChatbotName);
         System.out.println("What can I do for you?");
-        System.out.println("--------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("--------------------------");
+        System.out.println("___________________________");
+
+        while (true) {
+            String input = sc.nextLine();
+
+            if(input.equalsIgnoreCase("bye")) {
+                System.out.println("___________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("___________________________");
+                break;
+            }
+            else {
+                System.out.println("___________________________");
+                System.out.println(input);
+                System.out.println("___________________________");
+            }
+        }
+
+        sc.close();
     }
 }
