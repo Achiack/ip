d159ca9d58dc2f741759f55109fce3bd2b0ec0b0
Achiack <70217903+Achiack@users.noreply.github.com>
2026-02-03 22:50:11 +0800

    Improve styling for dialog box

--- src/main/java/Monad/ui/Monad.java
+++ src/main/java/Monad/ui/Monad.java
@@ -53,6 +53,13 @@ public class Monad {
         }
     }
 
+    /**
+     * Generates a response for the user's chat message.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
+
     public static void main(String[] args) {
         new Monad("data/monad.txt").run();
     }
