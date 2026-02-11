a321f46d4ced3fa70eff3ec89390c1a33c6961b2
Achiack <70217903+Achiack@users.noreply.github.com>
2026-02-11 15:40:55 +0800

    Remove the printing of line breaks in the UI, uses a printBox method instead

--- src/main/java/Monad/ui/Ui.java
+++ src/main/java/Monad/ui/Ui.java
@@ -3,6 +3,8 @@ package Monad.ui;
 import Monad.tasks.Task;
 import Monad.tasks.TaskList;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -124,22 +126,23 @@ public class Ui {
     }
 
     public void showFindResults(TaskList tasks, String keyword) {
-        System.out.println("____________________________________________");
-        System.out.println("Here are the matching tasks in your list:");
+        List<String> lines = new ArrayList<>();
+        lines.add("Here are the matching tasks in your list:");
 
         int count = 0;
         for (int i = 0; i < tasks.size(); i++) {
             Task task = tasks.get(i);
             if (task.getDescription().contains(keyword)) {
                 count++;
-                System.out.println(count + "." + task);
+                lines.add(count + "." + task);
             }
         }
 
         if (count == 0) {
-            System.out.println("No matching tasks found.");
+            lines.add("No matching tasks found.");
         }
-        System.out.println("____________________________________________");
+
+        printBox(lines.toArray(new String[0]));
     }
 
 }
